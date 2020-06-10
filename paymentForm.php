3d23d0ac8f539c4743e06ef81aece9b45c23bef4
krizcodz <krizcodz@users.noreply.github.com>
2020-06-10 17:08:43 +0400

    Update paymentForm.php

--- paymentForm.php
+++ paymentForm.php
@@ -371,9 +371,7 @@
 			<div class="success"></div>
 			<div class="successextra"></div>
 			<div class="error"></div>
-			<div class="text">For more test cards, please checkout this <a class="link" target="_new"
-																		   href="https://www.simplify.com/commerce/docs/tutorial/index#testing">page.</a>
-			</div>
+		
 		</div>
 	</form>
 </div>
